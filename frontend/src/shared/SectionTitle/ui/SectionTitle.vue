9a7a779e231022d34b0de097203e541c20f3d7e2
HABAB0 <tolya.shaprove@mail.ru>
2025-11-25 21:36:20 +0700

    add: SectionTitle component

---
+++ frontend/src/shared/SectionTitle/ui/SectionTitle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h2 class="title">
+    <slot></slot>
+  </h2>
+</template>
+<style lang="scss" scoped>
+  @use '@styles/_variables' as *;
+  @use '@styles/_mixins' as *;
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    @include text-style(headlines);
+    color: $text-primary;
+  }
+</style>
