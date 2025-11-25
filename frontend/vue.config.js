11357e04a6ceccad6c18a666428e312d00ef6f27
lisantov <lisantov@yandex.ru>
2025-11-25 20:55:12 +0700

    Add: config for scss

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       sass: {
         additionalData: `
           @import "@/app/assets/styles/_variables.scss";
+          @import "@/app/assets/styles/_mixins.scss";
         `
       }
     }
