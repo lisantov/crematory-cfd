0cae80cefabdf8bd380137d4c7011f05a79e5b66
lisantov <lisantov@yandex.ru>
2025-11-27 19:12:21 +0700

    Add: password field and new variables. Minor fix for accordeon

---
+++ frontend/src/shared/PasswordField/index.ts
@@ -0,0 +1,2 @@
+import PasswordField from './ui/PasswordField.vue';
+export default PasswordField;
---
+++ frontend/src/shared/PasswordField/ui/PasswordField.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import CardFrame from '@/shared/CardFrame'
+import { ref } from 'vue'
+
+interface IProps {
+  placeholder: string;
+}
+defineProps<IProps>()
+const model = defineModel<string>()
+const isHidden = ref<boolean>(true);
+
+const toggleHidden = () => isHidden.value = !isHidden.value;
+</script>
+
+<template>
+  <card-frame>
+    <div class="password-container">
+      <label>
+        <input
+          :type="isHidden
+            ? 'password'
+            : 'text'"
+          v-model="model"
+          class="password-field"
+          :placeholder="placeholder"
+        >
+      </label>
+      <button @click="toggleHidden" class="password-icon" :style="{padding: isHidden ? '3px 0 3px' : '0'}">
+        <img
+          :src="isHidden
+            ? '/assets/icons/eye.svg'
+            : '/assets/icons/eye-closed.svg'"
+          :alt="isHidden
+            ? 'Иконка для просмотра пароля'
+            : 'Иконка для скрытия пароля'"
+        >
+      </button>
+    </div>
+  </card-frame>
+</template>
+
+<style scoped lang="scss">
+  .password-container {
+    width: 100%;
+    display: grid;
+    padding: 12px 30px;
+    border-radius: 32px;
+    grid-template-columns: 1fr auto;
+    gap: 16px;
+
+    &:has(:focus-visible) {
+      outline: 2px solid $button-accent;
+    }
+  }
+
+  .password-field {
+    @include text-style(help);
+    width: 100%;
+    height: 100%;
+    border: none;
+    background-color: transparent;
+    color: $text-primary;
+    caret-color: $input-active-caret;
+    transition: color 0.25s ease-in-out;
+
+    &::placeholder {
+      color: $input-placeholder;
+      transition: color 0.15s ease-in-out;
+    }
+
+    &:hover::placeholder {
+      color: $input-hover-placeholder;
+    }
+
+    &:focus {
+      outline: none;
+
+      &::placeholder {
+        color: transparent;
+      }
+    }
+  }
+
+  .password-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    aspect-ratio: 1;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+
+    & img {
+      width: 100%;
+    }
+  }
+</style>
