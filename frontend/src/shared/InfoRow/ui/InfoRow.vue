416e0c8e80c341d1544e6550c43f544e6cb9388f
HABAB0 <tolya.shaprove@mail.ru>
2025-12-04 21:58:20 +0700

    add: component info row

---
+++ frontend/src/shared/InfoRow/ui/InfoRow.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+interface Iprops {
+  title: string
+}
+defineProps<Iprops>();
+</script>
+
+<template>
+<div class="info-row">
+  <p class="info-row-title">
+    {{ title }}
+  </p>
+  <p class="info-row-description">
+    <slot></slot>
+  </p>
+</div>
+</template>
+
+<style scoped lang="scss">
+.info-row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 4px 10px 2px $card-frame-shadow;
+  border-radius: 33px;
+  padding: 16px 105px;
+  color: $text-secondary;
+  @include text-style(info);
+  outline: 1px solid $card-frame-border;
+  transition: opacity 0.2s ease;
+
+  &-description {
+    opacity: 0.8;
+  }
+
+  &:hover {
+    outline: 2px solid $card-frame-border;
+
+    &:hover .info-row-title {
+      opacity: 0.8;
+    }
+
+    &:hover .info-row-description {
+      opacity: 1;
+    }
+  }
+}
+</style>
